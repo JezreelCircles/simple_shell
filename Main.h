b403ecb1f0cbc51443229d62fdf35468e5d4cfb7
JezreelCircles <chisomorehemagondwe@gmail.com>
2024-03-09 09:39:35 -0800

    mainnnn

--- Main.h
+++ Main.h
@@ -25,7 +25,7 @@ int handle_path(char *input, char **, char *, char *, int);
 int token_input(char input[], char **args, char *, char *, int);
 int child_process(char **args, char *);
 void nonInteractive(char *);
-char *_strtok(char *str, const char *delimiters);
+char _strtok(char *str, const char *delimiters);
 void _getenv(void);
 int Process(char *tken, int handle, char **args, char *cd, char *);
 void commentAfterHash(char str[]);
